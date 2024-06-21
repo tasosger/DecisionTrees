97a89fc6c38f3b109e8eccae74295eac483aa146
tasosger <tassos.gerassis@gmail.com>
2024-06-21 09:24:22 +0300

    solved issue by using promt engineering

--- id3.cpp
+++ id3.cpp
@@ -13,16 +13,16 @@ public:
     Node(int attr = -1, double val = 0.0, int res = -1) : attribute(attr), value(val), result(res) {}
 };
 
-vector<vector<double>> readCSV(const string& filename) {
-    vector<vector<double>> data;
+vector<vector<string>> readCSV(const string& filename) {
+    vector<vector<string>> data;
     ifstream file(filename);
     string line, cell;
     
     while (getline(file, line)) {
         stringstream lineStream(line);
-        vector<double> row;
+        vector<string> row;
         while (getline(lineStream, cell, ',')) {
-            row.push_back(stod(cell));
+            row.push_back(cell);
         }
         data.push_back(row);
     }
@@ -65,8 +65,63 @@ double informationGain(const vector<int>& y, const vector<vector<int>>& splits) 
     
     return total_entropy - weighted_entropy;
 }
+vector<vector<double>> encodeData(const vector<vector<string>>& rawData, vector<int>& y) {
+    unordered_map<string, int> encodings;
+    int next_code = 0;
+    vector<vector<double>> encodedData;
+    
+    for (const auto& row : rawData) {
+        vector<double> encodedRow;
+        for (size_t i = 0; i < row.size() - 1; ++i) {
+            if (encodings.find(row[i]) == encodings.end()) {
+                encodings[row[i]] = next_code++;
+            }
+            encodedRow.push_back(encodings[row[i]]);
+        }
+        encodedData.push_back(encodedRow);
+        
+        // Handle the last column as the target variable
+        if (encodings.find(row.back()) == encodings.end()) {
+            encodings[row.back()] = next_code++;
+        }
+        y.push_back(encodings[row.back()]);
+    }
+    
+    return encodedData;
+}
 
 
 int main() {
+    string filename = "./loan.csv";
+    vector<vector<string>> rawData = readCSV(filename);
+    
+    if (rawData.empty()) {
+        cout << "Failed to read the CSV file or the file is empty." << endl;
+        return -1;
+    }
+
+    vector<int> y;
+    vector<vector<double>> X = encodeData(rawData, y);
+
+   
+    cout << "First 5 rows of X:" << endl;
+    for (size_t i = 0; i < min(size_t(5), X.size()); ++i) {
+        for (size_t j = 0; j < X[i].size(); ++j) {
+            cout << X[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "First 5 elements of y:" << endl;
+    for (size_t i = 0; i < min(size_t(5), y.size()); ++i) {
+        cout << y[i] << " ";
+    }
+    cout << endl;
+
+    set<int> attributes;
+    for (size_t i = 0; i < X[0].size(); ++i) {
+        attributes.insert(i);
+    }
+
     return 0;
 }
