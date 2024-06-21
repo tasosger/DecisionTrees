82816ede63e9ea2c5362e99dffda14a92242364c
tasosger <tassos.gerassis@gmail.com>
2024-06-21 09:06:25 +0300

    csv reder

--- id3.cpp
+++ id3.cpp
@@ -12,3 +12,29 @@ public:
 
     Node(int attr = -1, double val = 0.0, int res = -1) : attribute(attr), value(val), result(res) {}
 };
+
+vector<vector<double>> readCSV(const string& filename) {
+    vector<vector<double>> data;
+    ifstream file(filename);
+    string line, cell;
+    
+    while (getline(file, line)) {
+        stringstream lineStream(line);
+        vector<double> row;
+        while (getline(lineStream, cell, ',')) {
+            row.push_back(stod(cell));
+        }
+        data.push_back(row);
+    }
+    
+    return data;
+}
+
+
+vector<double> getColumn(const vector<vector<double>>& data, int col) {
+    vector<double> column;
+    for (const auto& row : data) {
+        column.push_back(row[col]);
+    }
+    return column;
+}
