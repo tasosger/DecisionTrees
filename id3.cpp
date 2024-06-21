e448f8b7a37df54284944a464508e6a3d74a1691
tasosger <tassos.gerassis@gmail.com>
2024-06-21 09:05:17 +0300

    node

--- id3.cpp
+++ id3.cpp
@@ -1,3 +1,14 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+
+class Node {
+public:
+    int attribute;
+    double value;
+    int result;
+    map<double, Node*> children;
+
+    Node(int attr = -1, double val = 0.0, int res = -1) : attribute(attr), value(val), result(res) {}
+};
